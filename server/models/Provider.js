66ce66988d14e418f863048806b656a2ce4322a4
AipuAmeh <aipuameh.16@gmail.com>
2024-05-11 18:38:08 -0400

    created provider schema, hashed password

---
+++ server/models/Provider.js
@@ -0,0 +1,45 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const providerSchema = new Schema({
+    providerName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        length: 8,
+    },
+    patients: {
+        type: Schema.Types.ObjectId,
+        ref: 'Patient'
+    }
+});
+
+// if password is new or modified, hash it
+providerSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    };
+
+    next();
+});
+
+// if password is correct, compare it with password already on file
+providerSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password)
+ };
+
+const Provider = model('Provider', providerSchema);
+
+export default Provider;
