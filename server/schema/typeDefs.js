241c82f767eaa136d9de3868cf523a72258ff998
AipuAmeh <aipuameh.16@gmail.com>
2024-05-14 16:19:56 -0400

    defined patient and resolver model schemas in typedefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,5 +1,22 @@
 const typeDefs = `#graphql
-    
+
+type Provider {
+    _id: ID
+    providerName: String
+    email: String
+    password: String
+    patients: [Patient]!
+}
+
+type Patient {
+    _id: ID
+    firstName: String
+    lastName: String
+    dob: String
+    userName: String
+    email: String
+    password: String
+}
 `;
 
 export default typeDefs;
