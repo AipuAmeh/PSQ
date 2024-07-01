1588be4813fe712508db239eea88acc4224236aa
AipuAmeh <aipuameh.16@gmail.com>
2024-07-01 11:31:50 -0400

    working on styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { 
-  Box, 
-  Flex, 
-  Heading, 
-  Spacer, 
-  // useBreakpointValue 
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom';
-import { useCurrentUserContext } from '../utils/context/CurrentUser';
-
+import {
+  Box,
+  Flex,
+  Heading,
+  Spacer,
+  Image,
+  // useBreakpointValue
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useCurrentUserContext } from "../utils/context/CurrentUser";
 
 const Header = () => {
   const { isLoggedIn, logoutUser } = useCurrentUserContext();
@@ -15,34 +15,45 @@ const Header = () => {
   const logout = (e) => {
     e.preventDefault();
     logoutUser();
-  }
+  };
   // mobile development
   // const isMobile = useBreakpointValue({ base: true, md: false, sm: false });
 
   // add image for header
 
-return (
-<Flex minWidth='max-content'  p={4}  >
-  <Box p='2'>
-    <Heading 
-    size='md'> <Link to='/'> PsychQuiConsult</Link></Heading>
-  </Box>
-  <Spacer gap={15} />
-<Box display='flex' justifyContent='flex-end' gap={2} mr={4}>
-  { isLoggedIn() ? (
-  <button onClick={logout}>Logout</button> ) : (
-  <>
-    <h2>
-    <Link to='/signup'>Sign Up</Link>
-    </h2>
-    <h2>
-    <Link to='/login'>Log in</Link>
-    </h2>
-    </> )
-  }
-
-</Box>
-</Flex>
-)}
+  return (
+    <Flex minWidth="max-content" p={4}>
+      <Box >
+        <Heading size="md">
+          {" "}
+          <Link to="/">
+            <Image
+              boxSize="40px"
+              src="src/images/logo-trans.png"
+              alt="psychquiconsult-logo"
+              className="brand-logo-homepage"
+           
+            ></Image>
+          </Link>
+        </Heading>
+      </Box>
+      <Spacer gap={15} />
+      <Box display="flex" justifyContent="flex-end" gap={2} mr={4}>
+        {isLoggedIn() ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <>
+            <h2>
+              <Link to="/signup">Sign Up</Link>
+            </h2>
+            <h2>
+              <Link to="/login">Log in</Link>
+            </h2>
+          </>
+        )}
+      </Box>
+    </Flex>
+  );
+};
 
 export default Header;
