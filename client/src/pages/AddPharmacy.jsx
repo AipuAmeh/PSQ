0db687d2d7a6597ae298272941864a042897391e
AipuAmeh <aipuameh.16@gmail.com>
2024-08-28 14:01:12 -0400

    Added mutation to add pharmacy to patient form and routed to dashbaord, added validation for input fields

--- client/src/pages/AddPharmacy.jsx
+++ client/src/pages/AddPharmacy.jsx
@@ -7,20 +7,27 @@ import {
   InputLeftAddon,
   InputGroup,
   Text,
+  useToast
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { ADD_PHARMACY } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+
 
 const AddPharmacy = () => {
   const { id } = useParams();
-//   console.log("PATIENTS ID: " + id);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [addPharmacy] = useMutation(ADD_PHARMACY);
   const [formState, setFormState] = useState({
     patientId: id,
     pharmacyName: '',
     address: '',
     state: '',
     zipcode: 0,
-    phone: 0
+    phone: ""
 
   });
 
@@ -32,8 +39,73 @@ const AddPharmacy = () => {
    });
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     console.log(formState);
+    // validation for empty fields
+    if (formState.pharmacyName === "" || formState.address === "" || formState.state === "" || parseInt(formState.zipcode) === 0 || formState.phone === "") {
+      return toast({
+        title: "Error",
+        description: "Please complete all fields.",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+        // validation for zipcode 
+        const zipcodeCheck = formState.zipcode;
+        if (zipcodeCheck.length > 5) {
+          console.log(' too long');
+          return toast({
+            title: "Error",
+            description: "Zipcode not found",
+            status: "error",
+            duration: 2000,
+            isClosable: true,
+          });
+        }
+      // validation for phone number
+      const numberCheck = formState.phone;
+      console.log(numberCheck.length);
+      if (numberCheck.length > 10 || numberCheck.length < 10) {
+        return toast({
+          title: "Error",
+          description: "Please enter a valid pharmacy phone number.",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      }
+      // regex for checking if string is composed of all strings
+      const onlyDigits = (str) => {
+        return /^\d+$/.test(str);
+      };
+
+      if (!onlyDigits(numberCheck)) {
+        return toast({
+          title: "Error",
+          description: "Please enter a valid pharmacy phone number.",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      }
+
+    try {
+      const dataResponse = await addPharmacy({
+        variables: { 
+          patientId: id,
+          pharmacyName: formState.pharmacyName,
+          address: formState.address,
+          state: formState.state,
+          zipcode: parseInt(formState.zipcode),
+          phone: formState.phone
+         }
+      });
+      navigate('/dashboard');
+     return dataResponse;
+    } catch (error) {
+      console.log(error);
+        }
   }
 
   return (
@@ -41,7 +113,7 @@ const AddPharmacy = () => {
       <Text fontSize="3xl" mt={6}>
         Pharmacy Details
       </Text>
-      <FormControl mt={8}>
+      <FormControl mt={8} isRequired>
         <FormLabel>Pharmacy Name</FormLabel>
         <Input placeholder="Pharmacy Name" onChange={handleChange} name='pharmacyName'/>
 
@@ -49,14 +121,14 @@ const AddPharmacy = () => {
         <Input placeholder="Pharmacy Address" onChange={handleChange} name='address'/>
 
         <Box display="flex" justifyContent="center" gap={4}>
-          <FormControl>
+          <FormControl isRequired>
             <FormLabel mt={4}>State</FormLabel>
             <Input placeholder="e.g. Washington" onChange={handleChange} name='state'/>
           </FormControl>
 
-          <FormControl>
+          <FormControl isRequired>
             <FormLabel mt={4}>Zipcode</FormLabel>
-            <Input w="80%" placeholder='99999' onChange={handleChange} name='zipcode'/>
+            <Input w="80%" placeholder='99999' onChange={handleChange} name='zipcode' type='number'/>
           </FormControl>
         </Box>
         <FormLabel mt={4} >Phone</FormLabel>
