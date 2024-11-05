87d322da6f6fae091238d382d72c33e7f4fd8005
AipuAmeh <aipuameh.16@gmail.com>
2024-11-05 14:52:36 -0500

    added a contact block

--- client/src/components/ContactUsBlock.jsx
+++ client/src/components/ContactUsBlock.jsx
@@ -1,25 +1,21 @@
-import { Box, FormControl, Text, Input, Textarea } from "@chakra-ui/react";
+import { Box, Text,Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom"
 
 
 
 const ContactUsBlock = () => {
 return (
-    <Box display='flex' justifyContent='center' bg='brand.mintCream' my='3em' borderColor='brand.coolGray'>
-        <Box w='40%'>
-            <Text fontSize='2em'> Get In Touch With Us</Text>
-            <Text>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Magna maecenas commodo blandit rhoncus; condimentum nulla eu. Sed lobortis netus dis, metus et dis? Nisi donec malesuada amet congue bibendum nibh fusce. Et metus tempor metus magna mauris efficitur leo. Ullamcorper finibus habitasse volutpat erat mi ultricies magna luctus neque. Venenatis cras et efficitur accumsan fusce ornare aenean.
-            </Text>
-           
-        </Box>
-        <Box w='40%'>
-            <FormControl>
-                <Input />
-                <Input />
-                <Textarea />
-            </FormControl>
-        </Box>
+    <Box display='flex' justifyContent='center'>
+<Box display='flex' flexDirection='column' alignItems='center' py='1.5em' bg='brand.cambridgeBlue' w='90%' p='3em'>
+ <Text fontSize='1em' mb='1em'>INTERESTED IN LEARNING MORE?</Text>
+ <Text fontSize='1.5em' fontWeight='bold' mb='1em'>Schedule a Consultation Now</Text>
+ <Link to='/contact'>
+ <Button w='fit-content' size='lg' bg='brand.mintCream'> Contact Us Today!</Button>
+ </Link>
+
+</Box>
     </Box>
+
 )
 
 }
