75cdbc5d5beab670cfe2b5e35dfcde9b1e450b66
AipuAmeh <aipuameh.16@gmail.com>
2024-12-02 17:11:16 -0500

    made outline of form for contact block

--- client/src/components/ContactUsBlock.jsx
+++ client/src/components/ContactUsBlock.jsx
@@ -1,22 +1,30 @@
-import { Box, FormControl, Text, Input, Textarea } from "@chakra-ui/react";
+import { Box, FormControl, Text, Input, Textarea, FormLabel, FormHelperText, Button } from "@chakra-ui/react";
 
 
 
 const ContactUsBlock = () => {
 return (
-    <Box display='flex' justifyContent='center' bg='brand.mintCream' my='3em' borderColor='brand.coolGray'>
+    <Box display='flex' justifyContent='center' bg='brand.cambridgeBlue' py='3em' borderColor='brand.coolGray' gap='3em'>
         <Box w='40%'>
             <Text fontSize='2em'> Get In Touch With Us</Text>
-            <Text>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Magna maecenas commodo blandit rhoncus; condimentum nulla eu. Sed lobortis netus dis, metus et dis? Nisi donec malesuada amet congue bibendum nibh fusce. Et metus tempor metus magna mauris efficitur leo. Ullamcorper finibus habitasse volutpat erat mi ultricies magna luctus neque. Venenatis cras et efficitur accumsan fusce ornare aenean.
+            <Text mt='5em'>
+            Leave us a message. We will be sure to get back to you.
             </Text>
            
         </Box>
         <Box w='40%'>
-            <FormControl>
-                <Input />
-                <Input />
-                <Textarea />
+            <FormControl mt='3em' isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input placeholder='First and Last Name' color='white'/>
+                <FormLabel mt='0.5em'>Email</FormLabel>
+                <Input placeholder='Email' />
+                <FormHelperText>We&lsquo;ll never share your email.</FormHelperText>
+                <FormLabel mt='0.5em'>Message</FormLabel>
+                <Textarea placeholder='How Can We Help?'/>
+                <Box mt={6}>
+                <Button>Send Message</Button>
+                </Box>
+    
             </FormControl>
         </Box>
     </Box>
