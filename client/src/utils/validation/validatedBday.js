dba67734f1842bd34e433fb63feab91c6de45794
AipuAmeh <aipuameh.16@gmail.com>
2025-01-29 15:23:59 -0500

    created validated bday function for dob

--- client/src/utils/validation/validatedBday.js
+++ client/src/utils/validation/validatedBday.js
@@ -1,9 +1,11 @@
-export const isValidBday = (bday) => {
-    // date is a string on client side
-    // convert to date 
-    // check to make sure date cant be too far back or more than current year
-    // if year is more than current year, error
-    // if year is too far back, error
-    // if month is over 12 error
-    // if day is over 31 error - depending on the month
+export const isInvalidBday = (bday) => {
+    const currentYear = new Date().getFullYear();
+    const birthDate = new Date(bday);
+    const bdayYear = birthDate.getFullYear();   
+    // if bday year is greater than current year, error
+if (bdayYear < currentYear) {
+    return false;
+} else {
+    return true;
+}
 }
